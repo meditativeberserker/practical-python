223b1633c1da6c55cc9cbdc8815ad1e802fe193b
Roman <giner2005@gmail.com>
2021-05-26 19:28:22 +0500

    Numbers

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,17 @@
-# mortgage.py
-#
-# Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+year = 0
+
+while principal > 0:
+    if year <= 12:
+        principal = principal * (1+rate/12) - (payment + 1000)
+        total_paid = total_paid + (payment + 1000)
+        year += 1
+
+    else:
+        principal = principal * (1+rate/12) - payment
+        total_paid = total_paid + payment
+
+print('Total paid:', round(total_paid, 2))
